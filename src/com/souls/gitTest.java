fda0baa801edad3d90e64cc60da0b252cc59683c
pujingkang <58412001+pujingkang@users.noreply.github.com>
2022-08-29 01:27:12 +0800

    second

--- src/com/souls/gitTest.java
+++ src/com/souls/gitTest.java
@@ -3,5 +3,6 @@ package com.souls;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("first git!");
+        System.out.println("second git!");
     }
 }
